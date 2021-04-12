80278874fafb42c37b044ec6b56ef10d924ee472
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-04-12 09:49:29 -0400

    [components] importing AudioMotionAnalyzer to React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import AudiomotionAnalyzer from './components/AudiomotionAnalyzer.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+      <AudiomotionAnalyzer />
       </header>
     </div>
   );
