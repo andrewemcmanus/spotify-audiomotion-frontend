38ce5cb1b7630d72b92e623180837f10fd2f1c0f
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-04-12 10:22:59 -0400

    [components] import vs. exploring rarified control

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import AudiomotionAnalyzer from './components/AudiomotionAnalyzer.js'
+// import AudiomotionAnalyzer from './components/AudiomotionAnalyzer.js';
+import AudioMotionAnalyzer from 'audiomotion-analyzer';
+import Analyzer from './components/Analyzer.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <AudiomotionAnalyzer />
+      <AudioMotionAnalyzer />
       </header>
     </div>
   );
--- src/components/Analyzer.js
+++ src/components/Analyzer.js
@@ -1,1 +1,3 @@
 import AudioMotionAnalyzer from 'audiomotion-analyzer';
+//
+const audioMotion = new AudiomotionAnalyzer 
