fddaf7e62289066e8b92c4b590d45db4ee843cdb
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-04-18 12:45:59 -0400

    objects not valid issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 // import AudiomotionAnalyzer from './components/AudiomotionAnalyzer.js';
-// import AudioMotionAnalyzer from 'audiomotion-analyzer';
-import Analyzer from './components/Analyzer.js'
+import AudioMotionAnalyzer from 'audiomotion-analyzer';
+// import Analyzer from './components/Analyzer.js'
+
+function Analyzer() {
+  const audioMotion = new AudioMotionAnalyzer(document.getElementById('container'), {
+    source: document.getElementById('audio')
+    }
+  );
+  return audioMotion;
+}
+
 
 function App() {
   return (
--- src/components/Analyzer.js
+++ src/components/Analyzer.js
@@ -1,8 +1,2 @@
-import AudioMotionAnalyzer from 'https://cdn.skypack.dev/audiomotion-analyzer?min';
-//
-const audioMotion = new AudiomotionAnalyzer(document.getElementById('container'), {
-    source: document.getElementById('audio')
-  }
-)
-
-export default audioMotion;
+import AudioMotionAnalyzer from 'audiomotion-analyzer';
+// import AudioMotionAnalyzer from 'https://cdn.skypack.dev/audiomotion-analyzer?min';
